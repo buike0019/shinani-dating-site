2017bd74422608f63de4194b6417d0343b1dce84
Your Name <you@example.com>
2025-09-22 20:13:50 +0100

    pull

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "@/components/auth/AuthForm"
+
+export default function AuthPage() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-black px-6">
+      <AuthForm />
+    </main>
+  )
+}
